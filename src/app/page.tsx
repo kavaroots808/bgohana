b4f471cc839be3a136662b0b2b640350b827e60b
Ammon Watene <kavaroots@gmail.com>
2025-11-28 22:12:55 +0000

    Hide the dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,27 +13,8 @@ export default function Home() {
         <aside className="w-80 hidden lg:block border-r shrink-0">
           <AppSidebar />
         </aside>
-        <main className="flex-1 overflow-x-auto p-4 md:p-6">
-          <Tabs defaultValue="tree" className="h-full flex flex-col">
-            <div className="flex justify-center">
-              <TabsList>
-                <TabsTrigger value="dashboard">
-                  <LayoutGrid className="w-4 h-4 mr-2" />
-                  Dashboard
-                </TabsTrigger>
-                <TabsTrigger value="tree">
-                  <Network className="w-4 h-4 mr-2" />
-                  Genealogy
-                </TabsTrigger>
-              </TabsList>
-            </div>
-            <TabsContent value="dashboard" className="flex-1 overflow-y-auto mt-4">
-              <PerformanceDashboard />
-            </TabsContent>
-            <TabsContent value="tree" className="flex-1 overflow-y-auto">
-              <GenealogyTree />
-            </TabsContent>
-          </Tabs>
+        <main className="flex-1 overflow-x-auto">
+          <GenealogyTree />
         </main>
       </div>
     </div>
