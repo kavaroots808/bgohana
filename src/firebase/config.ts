e7bd445638acf7aff0474c988b9c9833f7754985
Ammon Watene <kavaroots@gmail.com>
2025-11-29 02:30:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2509906330-47be3",
+  "appId": "1:84761557416:web:a6555dcbf08c54030d859d",
+  "apiKey": "AIzaSyDPrUkIqSXXgoyejhRWdjQ4jRKPQRftxe0",
+  "authDomain": "studio-2509906330-47be3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "84761557416"
+};
