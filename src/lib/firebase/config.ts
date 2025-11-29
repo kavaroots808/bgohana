129102933ea92e2a6cb864e4f4ea0a2429b3e4de
Ammon Watene <kavaroots@gmail.com>
2025-11-29 01:34:24 +0000

    File changes

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // IMPORTANT: This is a public configuration. Do not store secrets here.
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID"
+  "projectId": "studio-2509906330-47be3",
+  "appId": "1:84761557416:web:a6555dcbf08c54030d859d",
+  "apiKey": "AIzaSyDPrUkIqSXXgoyejhRWdjQ4jRKPQRftxe0",
+  "authDomain": "studio-2509906330-47be3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "84761557416"
 };
 
 // Initialize Firebase
